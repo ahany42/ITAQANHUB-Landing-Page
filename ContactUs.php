86e7a555b4d4d02d221310de9daf7b1bb3bf744a
Ahany42 <ahanyfathy42@gmail.com>
2024-05-22 20:33:14 +0300

    Contact Us: php and mysql completed

--- ContactUs.php
+++ ContactUs.php
@@ -1,23 +1,28 @@
 <?php
 $Name = $_POST['Name'];
 $Email = $_POST['Email'];
-$Message = $_POST['message'];
-echo $Name;
+$Message = $_POST['Message'];
+if(empty($Name) && empty($Email) && empty($Message)){
+echo"Fill all fields";
+}
+else{
+
+
 $conn = new mysqli('localhost', 'root', '', 'testitqan');
-
-
 if ($conn->connect_error) {
   die('Connection Failed: ' . $conn->connect_error);
+  echo"Connection Error";
 
 } else {
   $stmt = $conn->prepare("insert into contactus(Name, Email, Message, TimeStamp) values(?, ?, ?, NOW())");
   $stmt->bind_param("sss", $Name, $Email, $Message);
   if ($stmt->execute()) {
-    echo "Thank you! Your message has been sent.";
+    header('Location:Success.html');
   } else {
     echo "Error sending message: " . $stmt->error;
   }
   $stmt->close();
   $conn->close();
+}
 }
 ?>
